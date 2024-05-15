d384de2700fd44707c5eebdc883d5b58cfffae6f
Garois <jean-garice.denoncin@epitech.eu>
2024-05-15 18:19:32 +0200

    some help to start

---
+++ Regexs.py
@@ -0,0 +1,15 @@
+##
+## EPITECH PROJECT, 2024
+## Regex-Lessons
+## File description:
+## Regexs
+##
+
+import re
+
+def main():
+    return
+
+if __name__ == "__main__" :
+    if main() == 84 :
+        exit(84)
